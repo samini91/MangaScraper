847309da5739800b1f76dc7be42d43c13b8f9292
samini91 <samini91@gmail.com>
2023-08-18 01:32:33 +0000

    update compiler and add flake

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "YTMusicRipper";
+
+  inputs = {
+    nixpkgs.url      = "github:NixOS/nixpkgs/nixos-23.05";
+    flake-utils.url  = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          # inherit system overlays;
+        };
+      in
+        with pkgs;
+        {
+          devShells.default = mkShell {
+            buildInputs = [
+              # ghc
+              # haskell.compiler.ghc88
+              haskell.compiler.ghc94
+              stack
+              pkg-config
+              zlib
+            ];
+
+          };
+        }
+    );
+}
