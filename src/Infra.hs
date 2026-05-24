cdd188c8f77d9ffbcdc689d26d0637e17fa4edbb
samini91 <samini91@gmail.com>
2026-05-24 22:20:50 +0000

    feat: add driveConfig to Env type

--- src/Infra.hs
+++ src/Infra.hs
@@ -4,7 +4,9 @@
 module Infra
   where
 import System.Log.FastLogger
+import GoogleDrive (DriveConfig)
 
-data Env = Env {
-  logFunc :: LogStr -> IO()
+data Env = Env
+  { logFunc :: LogStr -> IO ()
+  , driveConfig :: Maybe DriveConfig
   }
